8269d1ab7374af3c3c044545b3aa900af56d0d68
akinator001 <aakashgandhi1279@gmail.com>
2020-11-29 09:55:10 +0530

    [aakash] Welcome to Hello World React app

---
+++ src/App2.js
@@ -0,0 +1,10 @@
+//import logo from './logo.svg';
+import './App.css';
+import React from "react"
+
+class App extends React.Component{
+  render (){
+    return <div><h1>Hello Aakash</h1></div>
+  }
+}
+export default App;
